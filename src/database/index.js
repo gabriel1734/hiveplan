3ccb056d13b7195b9ca4134144d1ee77d9e35f1f
gabriel1734 <gabrielfurniel.gc@outlook.com>
2024-09-22 22:28:09 -0300

    corrigido arquivo de banco de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,92 +1,91 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, RefreshControl } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
+import * as SQLite from 'expo-sqlite';
+import { Alert } from "react-native";
 
-const Agendamento = ({ horario, servico, cliente }) => {
-  return (
-    <View style={styles.agendamento}>
-      <Text style={styles.horario}>{horario}</Text>
-      <View style={styles.servicoCliente}>
-        <MaterialIcons name="pets" size={24} color="black" />
-        <View style={styles.info}>
-          <Text style={styles.infoText}>{servico}</Text>
-          <Text style={styles.infoText}>{cliente}</Text>
-        </View>
-      </View>
-      <View style={styles.acoes}>
-        <MaterialIcons name="notifications" size={24} color="yellow" />
-        <MaterialIcons name="more-vert" size={24} color="black" />
-      </View>
-    </View>
-  );
-};
 
-const CardAgendamentos = () => {
-  const [refreshing, setRefreshing] = useState(false);
-  const [agendamentos, setAgendamentos] = useState([
-    { horario: '10h:00s - 10h:30s', servico: 'Tosa e Banho', cliente: 'Juninho do Gás' },
-    { horario: '10h:30s - 12h:30s', servico: 'Corte e Progressiva', cliente: 'Claudinha' },
-    { horario: '13h:00s - 15h:30s', servico: 'Clareamento', cliente: 'Cleyton' }
-  ]);
 
-  const onRefresh = () => {
-    setRefreshing(true);
-    setTimeout(() => {
-      setRefreshing(false);
-    }, 2000);
-  };
 
-  return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={{ paddingBottom: 20 }}
-      showsVerticalScrollIndicator={true}
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }
-    >
-      {agendamentos.map((agendamento, index) => (
-        <Agendamento key={index} {...agendamento} />
-      ))}
-    </ScrollView>
-  );
-};
+export function create(){
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-  },
-  agendamento: {
-    backgroundColor: '#6D6B69',
-    padding: 15,
-    marginBottom: 5,
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.23,
-    shadowRadius: 2.62,
-    elevation: 4,
-  },
-  horario: {
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  servicoCliente: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  info: {
-      marginLeft: 10,
-      color: 'white',
-  },
-  infoText: {
-    color: 'white',
-  },  
-  acoes: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-  },
-});
+    const db = SQLite.openDatabaseSync('database.db');
+    
+    db.execSync(`
+        CREATE TABLE IF NOT EXISTS dboAgendamentos (
+         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ,
+         nomeCliente VARCHAR(200),
+         telCliente VARCHAR(50),
+         dataAgendamento TEXT, 
+         horaAgendamento TEXT,
+         tipoAgendamento INTEGER, 
+         descricao VARCHAR(300)
+         ); 
 
-export default CardAgendamentos;
+        CREATE TABLE IF NOT EXISTS dboTipoAgendamento(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ,
+        nomeTipo VARCHAR(200) NOT NULL,
+        descricao VARCHAR(300)
+        );
+        `);
+}
+    
+export function adicionarTipoAgendamento(nome, descricao){
+    const db = SQLite.openDatabaseSync('database.db');
+      try{
+        const result = db.runSync('INSERT INTO dboTipoAgendamento (nome, descricao) VALUES (?, ?)', [nome, descricao]);
+
+         if(result.changes > 0)
+             Alert.alert('sucesso');
+     }
+     catch{
+         console.log('erro');
+     }
+}
+
+export  function  adicionarAgendamento(data, hora, tipo, nome, tel, descricao ){
+
+    const db = SQLite.openDatabaseSync('database.db');
+try{
+   const result = db.runSync('INSERT INTO dboAgendamentos (dataAgendamento, horaAgendamento, tipoAgendamento, descricao, nomeCliente, telCliente) VALUES (?, ?, ?, ?, ?, ?)', [data,hora, tipo, descricao, nome, tel]);
+
+   if(result.changes > 0)
+        Alert.alert('sucesso');
+}
+catch{
+    console.log('erro');
+}
+
+}
+
+export function verTipoAgendamentos(){
+    const db = SQLite.openDatabaseSync('database.db');
+
+    const result = db.getAllSync( 'SELECT * from dboTipoAgendamento');
+  
+}
+
+export function verAgendamentos(){
+    const db = SQLite.openDatabaseSync('database.db');
+    
+    const result = db.getAllSync( 'SELECT * from dboAgendamentos ORDER BY (dataAgendamento)');
+    for (const item of result){
+        console.log(item.id, item.nomeCliente, item.dataAgendamento, item.telCliente);
+    }
+        
+ };
+
+
+export function excluirAgendamento(id){
+    const db = SQLite.openDatabaseSync('database.db');
+
+    const result = db.runSync('DELETE FROM dboAgendamentos WHERE id = (?)', [id]);
+    if(result.changes > 0)
+        Alert.alert('sucesso');            
+            
+}
+
+export function excluirTipoAgendamento(id){
+    const db = SQLite.openDatabaseSync('database.db');
+
+    const result = db.runSync('DELETE FROM dboTipoAgendamento WHERE id = (?)', [id]);
+    if(result.changes > 0)
+        Alert.alert('sucesso');
+}
