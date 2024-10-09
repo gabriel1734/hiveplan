2b7ec4ab07c3ef46e1aa7fe9d6796b04a60b76c7
technomage2000 <lincoln.fagi@gmail.com>
2024-10-09 19:16:26 -0300

    teste modificações Colaboradores

--- src/pages/Colaboradores/index.js
+++ src/pages/Colaboradores/index.js
@@ -1,42 +1,37 @@
-import { SafeAreaView,CheckBox } from "react-native";
-import { View,useState,useEffect } from "react";
-import { TextInput } from "react-native-web";
-import { verTipoAgendamentos } from "../../database";
+import { SafeAreaView,CheckBox,TextInput } from "react-native";
+import { View,useState,useEffect, Text } from "react";
+//import { verTipoAgendamentos } from "../../database";
 
-export default function Colaboradores({navigation}){
+export default Colaboradores = ({navigation}) => {
 
     const [tiposAgendamentos, setTiposAgendamentos] = useState([]);
     const [isSelected, setSelection] = useState(false);
 
 
 
- const loadAgendamentos = () => {
+ /*const loadAgendamentos = () => {
     setTiposAgendamentos(verTipoAgendamentos());
   };
 
   useEffect(() => {
     loadAgendamentos();
-  },)
+  },)*/
 
 
     return(
-        <SafeAreaView>
-            <CheckBox
-                value={isSelected}
-                onValueChange={setSelection}
-                style={styles.checkbox}
-            />
+        <View>
+            
             <View>
                 <Text>Nome do colaborador:</Text>
                 <TextInput   placeholder="Nome do colaborador"/>
             </View>
-            {tiposAgendamentos.map((agendamento) => (
+            {/*tiposAgendamentos.map((agendamento) => (
                 <View key={agendamento.id} style={styles.agendamentoItem}>
                   <Text style={styles.agendamentoText}>{agendamento.nomeTipo}</Text>
                   <Text style={styles.agendamentoText}>{agendamento.descricao}</Text>
                 </View>
-              ))}
-        </SafeAreaView>
+              ))*/} 
+        </View>
     );
 }
 
