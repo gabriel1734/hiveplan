5d1ce893395d61693e906d53eec7357f0b3ca082
gabriel1734 <gabrielfurniel.gc@outlook.com>
2024-10-11 09:11:22 -0300

    ajuste de rota

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default function App() {
   return (
     <RootSiblingParent>
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Homer"
+      <Stack.Navigator initialRouteName="Home"
         screenOptions={{
           headerShown: false,
         }}
