5b111ab4b1cf3a76b26ad2515e23f755947b2d04
gabriel1734 <gabrielfurniel.gc@outlook.com>
2024-11-06 21:18:40 -0300

    imagens adicionadas

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,39 +1,67 @@
 // src/components/WelcomeModal.js
 import React, { useState } from 'react';
-import { View, Text, Modal, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Modal, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import PagerView from 'react-native-pager-view';
 
 const WelcomeModal = ({ visible, onClose }) => {
   const [pageIndex, setPageIndex] = useState(0);
 
+  const Img = [
+    {
+      acesso_menu: require('../../../assets/img/acesso_menu.png'),
+    },
+    {
+      menu: require('../../../assets/img/menu.png'),
+    },
+    {
+      agendamento: require('../../../assets/img/agendamento.png'),
+    },
+    {
+      colaboradores: require('../../../assets/img/colaboradores.png'),
+    },
+    {
+      servicos: require('../../../assets/img/servicos.png'),
+    },
+    {
+      configuracoes: require('../../../assets/img/configuracoes.png'),
+    },
+  ]
+
   const pages = [
     {
       title: "Bem-vindo ao App!",
       description: "Explore as principais funcionalidades do aplicativo.",
+      Image: null,
     },
     {
       title: "Acesso ao Menu",
       description: "Aperte no botão para acessar o menu principal.",
+      Image: Img.acesso_menu,
     },
     {
       title: "Menu",
       description: "Navegue pelas funcionalidades do aplicativo.",
+      Image: Img.menu,
     },
     {
       title: "Agendamentos",
       description: "Organize e visualize seus compromissos de forma fácil.",
+      Image: Img.agendamento,
     },
     {
       title: "Colaboradores",
       description: "Gerencie os colaboradores da sua equipe.",
+      Image: Img.colaboradores,
     },
     {
       title: "Serviços",
       description: "Adicione e edite serviços oferecidos.",
+      Image: Img.servicos,
     },
     {
       title: "Configurações",
       description: "Personalize o aplicativo de acordo com suas preferências e sua empres.",
+      Image: Img.configuracoes,
     },
   ];
 
@@ -55,6 +83,7 @@ const WelcomeModal = ({ visible, onClose }) => {
               <View key={index} style={styles.page}>
                 <Text style={styles.title}>{page.title}</Text>
                 <Text style={styles.description}>{page.description}</Text>
+                {page.Image ? <Image source={page.Image} style={{ width: 200, height: 200 }} /> : null}
               </View>
             ))}
           </PagerView>
