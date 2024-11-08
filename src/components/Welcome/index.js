464b0f4defb550491e633d94d7cab198cf9886a8
gabriel1734 <gabrielfurniel.gc@outlook.com>
2024-11-08 17:01:50 -0300

    finalizado modal de boas vindas

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,72 +1,57 @@
 // src/components/WelcomeModal.js
+import { Image } from 'expo-image';
 import React, { useState } from 'react';
-import { View, Text, Modal, Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import PagerView from 'react-native-pager-view';
-import { Animated } from 'react-native-web';
+import { View, Text, Modal, StyleSheet, TouchableOpacity } from 'react-native';
 
-const AnimatedPager = Animated.createAnimatedComponent(PagerView);
 
-const WelcomeModal = ({ visible, onClose }) => {
+const WelcomeModal = ({ visible, onClose, navigation }) => {
   const [pageIndex, setPageIndex] = useState(0);
 
-  const Img = [
-    {
-      acesso_menu: require('../../../assets/img/acesso_menu.png'),
-    },
-    {
-      menu: require('../../../assets/img/menu.png'),
-    },
-    {
-      agendamento: require('../../../assets/img/agendamento.png'),
-    },
-    {
-      colaboradores: require('../../../assets/img/colaboradores.png'),
-    },
-    {
-      servicos: require('../../../assets/img/servicos.png'),
-    },
-    {
-      configuracoes: require('../../../assets/img/configuracoes.png'),
-    },
-  ]
+  const handleClose = () => {
+    onClose();
+    navigation.navigate('Configuracao',{msg: 'Faça as configurações iniciais para começar a usar o aplicativo.'});
+  }
 
   const pages = [
     {
-      title: "Bem-vindo ao App!",
+      title: "Bem-vindo ao HivePlan!",
       description: "Explore as principais funcionalidades do aplicativo.",
-      Image: null,
+      Image: require('../../../assets/img/HIVEPLAN.png'),
     },
     {
       title: "Acesso ao Menu",
       description: "Aperte no botão para acessar o menu principal.",
-      Image: Img.acesso_menu,
+      Image: require('./../../../assets/img/acesso_menu.png'),
     },
     {
       title: "Menu",
       description: "Navegue pelas funcionalidades do aplicativo.",
-      Image: Img.menu,
+      Image: require('../../../assets/img/menu.png'),
     },
     {
       title: "Agendamentos",
       description: "Organize e visualize seus compromissos de forma fácil.",
-      Image: Img.agendamento,
+      Image: require('../../../assets/img/agendamento.png'),
     },
     {
       title: "Colaboradores",
       description: "Gerencie os colaboradores da sua equipe.",
-      Image: Img.colaboradores,
+      Image: require('../../../assets/img/colaboradores.png'),
     },
     {
       title: "Serviços",
       description: "Adicione e edite serviços oferecidos.",
-      Image: Img.servicos,
+      Image: require('../../../assets/img/servicos.png'),
     },
     {
       title: "Configurações",
-      description: "Personalize o aplicativo de acordo com suas preferências e sua empres.",
-      Image: Img.configuracoes,
+      description: "Personalize o aplicativo de acordo com suas preferências e sua empresa.",
+      Image: require('../../../assets/img/configuracoes.png'),
     },
   ];
+
+  
+
 
   return (
     <Modal
@@ -77,28 +62,28 @@ const WelcomeModal = ({ visible, onClose }) => {
     >
       <View style={styles.overlay}>
         <View style={styles.modalContent}>
-          <AnimatedPager
-            style={styles.pagerView}
-            initialPage={0}
-            onPageSelected={(e) => setPageIndex(e.nativeEvent.position)}
-          >
-            {pages.map((page, index) => (
-              <View key={index} style={styles.page}>
-                <Text style={styles.title}>{page.title}</Text>
-                <Text style={styles.description}>{page.description}</Text>
-                {page.Image ? <Image source={page.Image} style={{ width: 200, height: 200 }} /> : null}
-              </View>
-            ))}
-          </AnimatedPager>
+          <View style={styles.pagerView}>
+            <View style={styles.page}>
+              <Text style={styles.title}>{pages[pageIndex].title}</Text>
+              <Text style={styles.description}>{pages[pageIndex].description}</Text>
+              {pages[pageIndex].Image &&
+                <Image
+                  source={pages[pageIndex].Image}
+                  style={styles.Img}
+                  contentFit="cover"
+                  transition={1000}
+                />}
+            </View>
+          </View>
 
           <View style={styles.buttonContainer}>
             {pageIndex < pages.length - 1 ? (
               <TouchableOpacity onPress={() => setPageIndex(pageIndex + 1)} style={styles.btn}>
-                <Text>Próximo</Text>
+                <Text style={styles.btnText}>Próximo</Text>
               </TouchableOpacity>
             ) : (
-              <TouchableOpacity onPress={onClose} style={styles.btn}>
-                <Text>Fechar</Text>
+              <TouchableOpacity onPress={handleClose()} style={styles.btn}>
+                <Text  style={styles.btnText}>Fechar</Text>
               </TouchableOpacity>
             )}
           </View>
@@ -131,6 +116,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
+  Img: {
+    width: 200,
+    height: 400,
+    marginVertical: 20,
+  },
   title: {
     fontSize: 22,
     fontWeight: 'bold',
@@ -143,6 +133,16 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     padding: 10,
+  },
+  btn: {
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#6D6B69',
+    alignItems: 'center',
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 18,
   },
 });
 
